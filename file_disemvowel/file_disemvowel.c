45250438ec61bfcd598e312af3ac34c6fd477af7
Robert-Beane <beane031@morris.umn.edu>
2020-09-29 10:45:54 -0500

    add blank functions based on design suggestion

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -3,8 +3,23 @@
 
 #define BUF_SIZE 1024
 
-bool isAVowel(char c) { //Checks the given character if its a vowel or not
+bool is_vowel(char c) { //Checks the given character if its a vowel or not
   if (c == 'A' || c == 'a' || c == 'E' || c == 'e' || c == 'I' || c == 'i' || c == 'O' || c == 'o' || c == 'U' || c == 'u'){
     return true;
   } return false;
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+
+}
+
+int main(int argc, char *argv[]) {
+
+  disemvowel(inputFile, outputFile);
+  
+  return 0;
 }
