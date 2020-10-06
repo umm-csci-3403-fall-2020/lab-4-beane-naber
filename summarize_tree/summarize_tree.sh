9fd3a76e16219f9c2316e00de8f5801159c3a424
Robert-Beane <beane031@morris.umn.edu>
2020-10-06 11:35:44 -0500

    Implement summarize_tree.sh but tests are not passing.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,1 +1,16 @@
-starter
+#!/usr/bin/env bash
+
+HERE=$(pwd) #gets current position
+
+PATH="$1" #sets input to $PATH
+
+cd "$PATH" || exit #cds into the called path
+
+NUM_OF_FILES=$( find . -type f | wc -l)
+
+NUM_OF_DIRS=$( find . -type d | wc -l)
+
+echo "There were" "$NUM_OF_DIRS" "directories."
+echo "There were" "$NUM_OF_FILES" "regular files."
+
+cd "$HERE" || exit
